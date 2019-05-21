8814420919ff624c87bc03a44bed97654de596a4
armen <mnmagevorgyanweb@gmail.com>
2019-05-21 23:10:33 +0400

    array adapter

--- MenuProject/app/src/main/java/com/dba_droid/menuproject/MainActivity.java
+++ MenuProject/app/src/main/java/com/dba_droid/menuproject/MainActivity.java
@@ -5,9 +5,14 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] data = new String[] { "one", "two", "three" };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +21,18 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setTitle("My title");
+//            getSupportActionBar().setDisplayShowTitleEnabled(false);
             getSupportActionBar().setSubtitle("My subtitle");
             getSupportActionBar().setElevation(30);
+
+
+            Spinner spinner = findViewById(R.id.spinner);
+
+            ArrayAdapter<CharSequence> adapter;
+//                 adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, data);
+                 adapter = ArrayAdapter.createFromResource(this, R.array.dayofweek, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
         }
     }
 
